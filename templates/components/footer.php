5ee84c28b9013e5b239a12276382d5ed54be96ef
Dev.Ezra <92317816+HackEzra@users.noreply.github.com>
2024-01-03 10:07:36 +0300

    template files:)

---
+++ templates/components/footer.php
@@ -0,0 +1,11 @@
+
+
+<footer class="footer">
+    <div class="container text-center" style="padding: 10px;">
+        <span>© 2024 &nbsp;</span>
+        <img src="/images/car-logo.svg" />
+    </div>
+</footer>
+
+</body>
+</html>
